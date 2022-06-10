8922283d26dd53c2e88eed88b50f45479727dc47
KASHI TAMANG <tamang.kashi@gmail.com>
2022-06-10 09:48:54 -0700

    adds footer component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import backgroundImgDay from '../src/backgroundday.png';
 //import components
 import Header from './Components/Header/Header.js';
+import Footer from './Components/Footer/Footer.js';
 //import data 
 
 function App() {
   return (
     <div className="App" style={{ backgroundImage: `url(${backgroundImgDay})` }}>
       <Header/>
+      <Footer year="2022"/>
     </div>
   );
 }
