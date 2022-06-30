2c425fb38e075d8b47d696e72cdf2770bd9704d5
KASHI TAMANG <tamang.kashi@gmail.com>
2022-06-30 15:48:42 -0700

    adding table of community members

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import MyCharts from './MyCharts';
+import MyTable from './MyTable';
 
 export default function Admin() {
   return (
-    <div>This is the Admin Page</div>
+    <div>
+      <h1>Welcome to admin</h1>
+      <h3>Charts</h3>
+      <MyCharts/>
+
+      <MyTable/>
+    </div>
   );
 }
